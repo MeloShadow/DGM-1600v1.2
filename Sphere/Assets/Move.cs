55d36ec3b87ebba6ceee9c39fdcdce3d5e9db9d1
Unknown <dbmbc21@gmail.com>
2017-01-31 11:07:38 -0700

    Classes

---
+++ Sphere/Assets/Move.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))	
+		{
+			gameObject.transform.Translate (-.25f, 0, 0);
+		}	
+		if(Input.GetKey(KeyCode.RightArrow))	
+		{
+			gameObject.transform.Translate (.25f, 0, 0);
+		}	
+		if(Input.GetKey(KeyCode.UpArrow))	
+		{
+			gameObject.transform.Translate (0, .25f, 0);
+		}	
+		if(Input.GetKey(KeyCode.DownArrow))	
+		{
+			gameObject.transform.Translate (0, -.25f, 0);
+		}	
+		if(Input.GetKey(KeyCode.D))
+		{
+			gameObject.transform.Translate (Time.deltaTime, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.A))
+		{
+			gameObject.transform.Translate (-Time.deltaTime, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			gameObject.transform.Translate (0, Time.deltaTime, 0);
+		}
+		if(Input.GetKey(KeyCode.S))
+		{
+			gameObject.transform.Translate (0, -Time.deltaTime, 0);
+		}
+	}
+}
--- Sphere/Assets/changeColor.cs
+++ Sphere/Assets/changeColor.cs
@@ -6,23 +6,28 @@ public class changeColor : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//Make the color red
-		if(Input.GetKeyDown(KeyCode.R))	{
+		if(Input.GetKeyDown(KeyCode.R))	
+		{
 			GetComponent<Renderer> ().material.color = Color.red;
 		}
 		//Make the color Green
-		if(Input.GetKeyDown(KeyCode.G))	{
+		if(Input.GetKeyDown(KeyCode.G))	
+		{
 			GetComponent<Renderer> ().material.color = Color.green;
 		}
 		//Make the color Blue
-		if (Input.GetKeyDown(KeyCode.B))	{
+		if (Input.GetKeyDown(KeyCode.B))	
+		{
 			GetComponent<Renderer> ().material.color = Color.blue;
 		}
 		//Make the color Black
-		if (Input.GetKeyDown(KeyCode.Space))	{
+		if (Input.GetKeyDown(KeyCode.Space))	
+		{
 			GetComponent<Renderer> ().material.color = Color.black;
 		}
 		//Make the color White
-		if (Input.GetKeyDown(KeyCode.Backspace))	{
+		if (Input.GetKeyDown(KeyCode.Backspace))	
+		{
 			GetComponent<Renderer> ().material.color = Color.white;
 		}
 		//Make the color Yellow
