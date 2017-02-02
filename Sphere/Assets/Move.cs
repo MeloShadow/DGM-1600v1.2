050975699a02d773859f0473596bf32faead7676
Unknown <dbmbc21@gmail.com>
2017-02-02 11:24:49 -0700

    Variables

--- Sphere/Assets/Move.cs
+++ Sphere/Assets/Move.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Move : MonoBehaviour {
 	public float horizontalSpeed = .5f;
+	//Adding float will allow for a decimal and horizontalSpeed will be the name for it.
 	public float verticalSpeed = .2f;
-
+	//Same thing for this line
 
 
 	// Update is called once per frame
