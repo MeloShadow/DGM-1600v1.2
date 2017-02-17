c19827dbf14b5db8a1527f17fa65f80b9a985da5
Unknown <dbmbc21@gmail.com>
2017-02-16 21:59:16 -0700

    Switch statements

---
+++ Sphere/Assets/IfElseStatements.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfElseStatements : MonoBehaviour {
+	public int time = 10;
+	public float grade = 95;
+	// Use this for initialization
+	void Start () {
+		if (time < 12)
+		{
+			print("Good morning!");
+		}
+		else if (time == 12)
+		{
+			print("Lunch time!");
+		}
+		else if (time < 18)
+		{
+			print("Good afternoon!");
+		}
+		else 
+		{
+			print("Good evening!");
+		}
+		if (grade > 90)
+		{
+			print("A");
+		}
+		else if (grade > 80)
+		{
+			print("B");
+		}
+		else if (grade > 70)
+		{
+			print("C");
+		}
+		else 
+		{
+			print("What is going on?!");
+		}
+		
+		
+		
+	}
+}
---
+++ Sphere/Assets/SwitchStatements.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchStatements : MonoBehaviour {
+	public int time = 10;
+
+	private string creature = "dragon";
+	private string appearance;
+	private bool exists = true;
+
+	// Use this for initialization
+	void Start () {
+		//Create a switch statement to determine the time and what to say at that time 
+		/*	Letting the reader know what the appearance of the creature is like and what it sounds like.
+			If the case for the creature is true then it'll print out the appearance for that true statement.
+		 */
+		switch (creature)
+		{
+			case "dragon":
+				appearance = "Benedict Cumberbach!";
+				break;
+			case "chimera":
+				appearance = "a lionsnakebird type of thing...";
+				break;
+			case "pegasus":
+				appearance = "a glorious bird horse!";
+				break;
+			case "giant":
+				appearance = "a huge thing that steps on \"litle people\"";
+				break;
+			case "megaladon":
+				appearance = "a huge shark type of thing";
+				break;
+			case "ogre":
+				appearance = "a big ugly yet small giantusually green";
+				break;
+			default: 
+				exists = false;
+				break;
+		}
+		if (exists)
+		{
+			print ("A " + creature + " looks like " + appearance + ".");
+		}
+		else
+		{
+			print(creature + " does not exist, no I'm not lying!");
+		}
+	}
+}
